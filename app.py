5b03d1bf234d5a0b889ac84971e6e18a90ce42bf
riemannulus <riemannulus@hitagi.moe>
2018-02-26 08:41:54 +0900

    Make model

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+import os
 
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from .model import Base, User
+
+
+DATABASE_URI = os.getenv('DATABASE_URI')
 
 app = Flask(
     __name__,
@@ -7,6 +15,13 @@ app = Flask(
     static_folder='static/dist',
     template_folder='templates',
 )
+
+
+@app.before_first_request
+def initialize():
+    global Session
+    engine = create_engine(DATABASE_URI)
+    Session = scoped_session(sessionmaker(engine))
 
 
 @app.route('/')
