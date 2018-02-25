c718e107a335c9ab6b9986fd6db6eb712a741c03
kjwon15 <kjwonmail@gmail.com>
2018-02-26 06:29:49 +0900

    Setup frontend

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='static/dist',
+    template_folder='templates',
+)
 
 
 @app.route('/')
