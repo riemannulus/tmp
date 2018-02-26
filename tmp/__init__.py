12d67fd347e0961ef2cb8c858cccb730734875cd
kjwon15 <kjwonmail@gmail.com>
2018-02-26 19:52:37 +0900

    Save choice and redirect

--- tmp/__init__.py
+++ tmp/__init__.py
@@ -1,6 +1,7 @@
+from functools import wraps
 import os
 
-from flask import Flask, render_template
+from flask import Flask, abort, g, redirect, render_template, request, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -22,8 +23,68 @@ def initialize():
     global Session
     engine = create_engine(DATABASE_URI)
     Session = scoped_session(sessionmaker(engine))
+    Base.metadata.create_all(engine)
+    Base.query = Session.query_property()
+
+
+def use_db(f):
+
+    @wraps(f)
+    def wrapped(*args, **kwarg):
+        assert not hasattr(g, 'db')
+        g.db = Session()
+        return f(*args, **kwarg)
+
+    return wrapped
+
+
+@app.after_request
+def after_request(response):
+    if hasattr(g, 'db'):
+        g.db.close()
+
+    return response
+
+
+def get_ip():
+    forwarded = request.headers.getlist('X-Forwarded-For')
+    if forwarded:
+        return forwarded[0]
+
+    return request.remote_addr
 
 
 @app.route('/')
+@use_db
 def index():
+    ip = get_ip()
+    user = g.db.query(User).get(ip)
+    if user:
+        # TODO: Make these entry points.
+        if user.choice == 'blue':
+            return redirect('out')
+        elif user.choice == 'red':
+            return redirect('second')
+
     return render_template('index.html')
+
+
+@app.route('/choice')
+@use_db
+def choice():
+    color = request.args.get('choice')
+    if color not in ('blue', 'red'):
+        return abort(400)
+
+    ip = get_ip()
+
+    user = g.db.query(User).get(ip)
+    if user:
+        return redirect(url_for('index'))
+
+    user = User()
+    user.ip = ip
+    user.choice = color
+    g.db.add(user)
+    g.db.commit()
+    return redirect(url_for('index'))
