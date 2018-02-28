639973d28ac137b4885bc96b6d3e44728500e3e6
kjwon15 <kjwonmail@gmail.com>
2018-02-28 13:08:17 +0900

    Finalise matrix
    - Attach sound
    - Attach pill image
    - Goto sky if red
      Goto out if blue

--- tmp/matrix/__init__.py
+++ tmp/matrix/__init__.py
@@ -21,9 +21,9 @@ def index():
     if user and user.matrix:
         # TODO: Make these entry points.
         if user.matrix.choice == 'blue':
-            return redirect('out')
+            return redirect(url_for('.out'))
         elif user.matrix.choice == 'red':
-            return redirect('second')
+            return redirect(url_for('sky.index'))
 
     return render_template('matrix/index.html')
 
@@ -49,3 +49,8 @@ def choice():
     g.db.add(user)
     g.db.commit()
     return redirect(url_for('.index'))
+
+
+@matrix.route('/out')
+def out():
+    return render_template('matrix/out.html')
