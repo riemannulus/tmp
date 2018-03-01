112dfaadd9ea5288a8b98a4298aa48ed923e0cf9
kjwon15 <kjwonmail@gmail.com>
2018-03-02 02:54:40 +0900

    Implement image viewer

--- tmp/assets/qrcode.js
+++ tmp/assets/qrcode.js
@@ -1,1 +1,23 @@
 import './qrcode.scss';
+
+const imageContainer = document.querySelector('#large-image');
+
+function imageClickHandler(event) {
+    let img = event.target;
+    let newImg = document.createElement('img');
+    newImg.src = img.src;
+
+    imageContainer.innerHTML = null;
+    imageContainer.appendChild(newImg);
+    imageContainer.style.display = 'block';
+}
+
+function closeLargeImage() {
+    imageContainer.style.display = '';
+}
+
+for (let img of document.querySelectorAll('#plate .locked img')) {
+    img.addEventListener('click', imageClickHandler);
+}
+
+imageContainer.addEventListener('click', closeLargeImage);
